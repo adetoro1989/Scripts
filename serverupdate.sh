95b885e9c0a3edcfdacde78c712d5de82f65a3b7
tunji <aadetoro@jhpiego.org>
2025-05-03 15:33:05 +0100

    I will be pushing the new script

---
+++ serverupdate.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+# Description: This Script is to keep my server up to date regularly
+# Date Created: 03-MAY-2025
+# Date Modified: 03-MAY-2025
+
+echo
+sudo apt update -y && sudo apt upgrade -y
+echo
+echo "System Updated Successfully!"
