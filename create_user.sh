4d9db08b94fa275fe4e08776b82ea7628f44af32
tunji <aadetoro@jhpiego.org>
2025-05-09 14:57:45 +0100

    I just added some test files

---
+++ create_user.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+# Description: This script i to keep my server up to date regularly and keep alog of the update details on a daily basis into a log file
+# Date Created: 03-MAY-2025
+# Date Modified: 09-MAY-2025
+
+y=$(date +"%Y-%m-%d %H:%M:%S.%3N")
+echo
+sudo apt update && sudo apt upgrade -y >> /ubuntu/project/scripts/update.log
+echo
+echo "System updated successfully!" >> /ubuntu/project/scripts/update.log
+echo
+echo "See details of Update ran today: $y" >> /ubuntu/project/scripts/update.log
+echo
+cat update.log
+echo
---
+++ user_creation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo
+echo "Choose from any of the option as  seen: 1 === Add User, 2 === Show Syste>
+echo
+read -p "Enter any choice from the options above:" response
+if 
