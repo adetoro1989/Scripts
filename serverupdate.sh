190e201d1f6ff95f5f6372642553a98a526d22ef
tunji <aadetoro@jhpiego.org>
2025-05-09 04:41:43 +0100

    I have updated the serverupdate.sh file

--- serverupdate.sh
+++ serverupdate.sh
@@ -1,9 +1,14 @@
 #! /bin/bash
-# Description: This Script is to keep my server up to date regularly
+# Description: This script i to keep my server up to date regularly and keep alog of the update details on a daily basis into a log file
 # Date Created: 03-MAY-2025
-# Date Modified: 03-MAY-2025
+# Date Modified: 09-MAY-2025
 
+y=$(date +"%Y-%m-%d %H:%M:%S.%3N")
 echo
-sudo apt update -y && sudo apt upgrade -y
+sudo apt update && sudo apt upgrade -y >> /ubuntu/project/scripts/update.log
 echo
-echo "System Updated Successfully!"
+echo "System updated successfully!" >> /ubuntu/project/scripts/update.log
+echo
+echo "See details of Update ran today: $y" >> /ubuntu/project/scripts/update.log
+echo
+cat update.log
