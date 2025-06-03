3d3d993cb4149b2c056056a72c5e4f229377307c
tunji <aadetoro@jhpiego.org>
2025-06-03 11:35:41 +0100

    I created a script that generates a random secured password

---
+++ passgen.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+# Description: This script is a script that generates password
+# Date Created: 06-JUNE-2025
+# Date Modified: 06-JUNE-2025
+
+echo "The script will generate password in linux"
+
+password_gen=$(head -c 16 /dev/urandom | base64 |tr -dc 'a-zA-Z0-9')
+
+echo "Your new password is: $password_gen"
